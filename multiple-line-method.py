62f4c12c51fb1a74e3114708b6dd1461a84b3eed
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-04-23 14:07:20 +0800

    added pseudocode

--- multiple-line-method.py
+++ multiple-line-method.py
@@ -1,3 +1,4 @@
+# **PROGRAM 3**
 # Write a method in python to write multiple line of text contents into a text file mylife.txt.
 # See sample output:
 # Enter line: Beautiful is better than ugly.
@@ -6,3 +7,8 @@
 # Are there more lines y/n? y
 # Enter line: Simple is better than complex.
 # Are there more lines y/n? n
+
+# Define the method/function
+# Write into the output file 'mylife.txt'
+# Iterate input texts
+# Write input to 'mylife.txt'
